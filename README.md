071143beb18f578978f8a12bede7f852b7225f77
Parth Shandilya <parth.shandilya@cern.ch>
2022-06-16 11:03:25 +0200

    global: update README and remove unused views

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-## ML Artifact Repo
+# ML Artifact
+
+Proof Of Concept for the ML artifact repository
+
+## Development Instructions
+
+- Major Dependency requirements
+    1. Python 3
+    2. Django 4
+    3. Django Rest Framework 3
+
+- Local Setup
+
+    1. `python3 -m venv env`
+    2. `source env/bin/activate`
+    3. `pip install -r requirements.txt`
+    4. `python manage.py runserver`
+
+- Database migrations
+
+    1. `python manage.py makemigrations`
+    2. `python manage.py migrate`
--- mlartifact/urls.py
+++ mlartifact/urls.py
@@ -15,16 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework import routers
-from .views import UserViewSet, GroupViewSet
 
-
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-router.register(r'groups', GroupViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
     path('auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('artifacts/', include('artifacts.urls')),
     path('admin/', admin.site.urls),
