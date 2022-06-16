4c5ad049ae1f67a6d416d0d639e73d9a103ba3be
Parth Shandilya <parth.shandilya@cern.ch>
2022-06-16 14:50:53 +0200

    serializer: update create and update methods

--- artifacts/serializers.py
+++ artifacts/serializers.py
@@ -46,38 +46,12 @@ class MLModelSerializer(serializers.ModelSerializer):
 
 
 class MLDatasetVersionSerializer(serializers.ModelSerializer):
-    ml_dataset = MLDatasetSerializer()
-
     class Meta:
         model = MLDatasetVersion
         fields = ['id', 'url', 'version', 'ml_dataset', 'user']
-
-
-    def create(self, validated_data):
-        ml_dataset_data = validated_data.pop('ml_dataset')
-
-        ml_dataset_version = MLDatasetVersion.objects.create(**validated_data)
-        ml_dataset_version.save()
-
-        ml_dataset_version.ml_dataset = ml_dataset_data
-
-        return ml_dataset_version
 
 
 class MLModelVersionSerializer(serializers.ModelSerializer):
     class Meta:
         model = MLModelVersion
         fields = ['id', 'url', 'version', 'ml_model', 'ml_dataset_version', 'user']
-
-
-    def create(self, validated_data):
-        ml_model = validated_data.pop('ml_model')
-        ml_dataset_version = validated_data.pop('ml_dataset_version')
-
-        ml_model_version = MLModelVersion.objects.create(**validated_data)
-        ml_model_version.save()
-
-        ml_model_version.ml_model = ml_model
-        ml_model_version.ml_dataset_version = ml_dataset_version
-
-        return ml_model_version
